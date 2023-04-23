ecbe7deec24e3016a2e5014ad83b4070db3858b6
Argha Chakrabarty <ac380012@gmail.com>
2023-04-23 23:00:18 +0530

    init project

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+
+export default App;
---
+++ src/auth.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { View } from "react-native/types"
+
+const AuthView = () => {
+    return (
+        <View></View>
+    )
+}
+
+export default AuthView;
